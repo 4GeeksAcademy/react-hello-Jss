fd72a41871e8a9cecfaa04ed2d135840fc5a64a5
Sebastian Sarria <161655160+JSebasSN@users.noreply.github.com>
2024-06-28 10:36:06 +0000

    py terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [listaTarea, setlistaTarea] = useState([]);
+	const [tarea, setTarea] = useState("")
+
+
+	useEffect(() => {
+		fetchGetTask()
+	}, [])
+
+	/////////////////////////////////////////////////////
+	const handleChange = (e) => {
+		setTarea(e.target.value)
+	}
+
+	const handleKeyEnter = (e) => {
+		if (e.code === "Enter") {
+			fetchPostTask()
+		}
+	}
+
+	/////////////////////////////////////////////////////
+	const fetchGetTask = () => {
+		fetch('https://playground.4geeks.com/contact/agendas/sebas', {
+			method: "GET"
+		})
+			.then(response => response.json())
+			.then(data => setlistaTarea(data.contacts))
+			.catch(error => console.log(error))
+	}
+	/////////////////////////////////////////////////////
+	const fetchPostTask = () => {
+
+		const nuevaTarea = {
+
+			"name": tarea,
+			"phone": "",
+			"email": "",
+			"address": ""
+
+		}
+
+		fetch('https://playground.4geeks.com/contact/agendas/sebas/contacts', {
+			method: "POST",
+			body: JSON.stringify(nuevaTarea),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
+			.then(data => {
+				fetchGetTask()
+				setTarea("")
+			})
+			.catch(error => console.log(error))
+	}
+	/////////////////////////////////////////////////////
+	const fetchDeleteTask = (id) => {
+		fetch(`https://playground.4geeks.com/contact/agendas/sebas/contacts/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response)
+			.then(data => fetchGetTask())
+			.catch(error => console.log(error))
+	}
+
+
+	const tasksLeft = listaTarea.length
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="baseLista">
+			<div className="lista">
+				<input type="text" placeholder="Añade tareas" onChange={handleChange} value={tarea} onKeyDown={handleKeyEnter} />
+				<div className="pendientes">
+
+					{listaTarea.map((tareaItem, index) => (
+						<div className="txt" key={index}>
+							<p className="estiloTarea" >{tareaItem.name}</p>
+							<button className="delete-button" onClick={() => fetchDeleteTask(tareaItem.id)}>X</button>
+						</div>
+					))}
+
+				</div>
+				<div className="footer">
+					{tasksLeft} tarea{tasksLeft !== 1 ? 's' : ''} restantes
+				</div>
+			</div>
 		</div>
 	);
 };
